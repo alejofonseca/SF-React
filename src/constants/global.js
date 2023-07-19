acc118d0becf919fae6db83e5c6b29296e0e5884
Alejandro Fonseca <alejandrofonseca@gmx.com>
2023-07-19 22:29:22 +0200

    First commit from here

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,4 +1,4 @@
 const tkn = '00D0D000000D4Mh!AR0AQHopKKqfdpMkoqoru5Lz9LJLhNJaaboOCYOwuqh6sIB5ifrJUpxc6nlP3Dd5_4QVR4MpaA_R7frQxNzuLebAqpQpTXQ8';
 const secret_key = '0013W00000OHGX3QAP0053W000001UzCVQA01689768401'; // Never changes because it belongs to the account
- 
+//
 export {tkn, secret_key};
