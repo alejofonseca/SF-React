93e23ecea95b1966766fcd9f6acf3b1e1341cd5d
Alejandro Fonseca <alejofonseca@users.noreply.github.com>
2024-07-24 09:35:59 +0200

    Add files via upload

--- src/webpages/OrderProduct.js
+++ src/webpages/OrderProduct.js
@@ -4,6 +4,7 @@ import { Navbar, Nav, NavDropdown, Container, Table, Form, InputGroup, Button } 
 import logo from '../brose.svg';
 import { menu } from "../config/menu";
 import { fetchData } from "../helpers/api";
+import download_light from '../images/download_light.svg';
 
 /**
  * This app consumes the services exposed on QAFC apex class GetOrderProducts. The connection is done through the
@@ -237,10 +238,7 @@ const lineitems_location_arr = lineitems_location.split('.');
                             <td><div className="">
                                 {item.Attachments?.records[0].Id !== undefined &&
                                 <Button type="button" className="btn btn-light btn-sm" style={{'--bs-btn-padding-y': '.13rem', '--bs-btn-padding-x': '.3rem'}} onClick={(() => download(item.Attachments.records[0].Id))}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
-                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
-                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
-                                    </svg>
+                                    <img src={download_light} alt='download' />
                                 </Button>
                                 }
                             </div></td>
