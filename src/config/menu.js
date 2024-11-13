ff9694395f556817be00fac16de146e26fbd7063
Alejandro Fonseca <alejofonseca@users.noreply.github.com>
2024-11-13 16:52:47 +0100

    Add files via upload

---
+++ src/config/menu.js
@@ -0,0 +1,58 @@
+export const menu = [
+    {
+        label: 'Home',
+        to: '/firstspirit'
+    },
+    {
+        label: 'Orders',
+        to: '/orderproduct'
+    },
+    {
+        label: 'Bike list',
+        to: '/bikelist'
+    },
+    {
+        label: 'Complaint',
+        to: '/complaint'
+    }
+    /*
+    {
+        label: 'Bikes',
+        to: '/bikes',
+    },
+    {
+        label: 'Models',
+        submenu: [
+            {
+                label: 'View all',
+                to: '/bikes/1'
+            },
+            {
+                label: 'New Model',
+                to: '/'
+            }
+        ]
+    },
+    {
+        label: 'Components',
+        submenu: [
+            {
+                label: 'View all',
+                to: ''
+            },
+            {
+                label: 'New Drive Unit',
+                to: ''
+            },
+            {
+                label: 'New HMI',
+                to: ''
+            },
+            {
+                label: 'New Battery',
+                to: ''
+            }
+        ]
+    }
+    */
+];
